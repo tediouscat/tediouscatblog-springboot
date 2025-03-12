7d0019722026c67ede834b7e8a9fea2529922e04
tediouscat <tediouscat@126.com>
2025-03-12 12:33:00 +0800

    web-module add

---
+++ tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/TediouscatblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.tediouscat.tediouscatblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TediouscatblogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TediouscatblogWebApplication.class, args);
+    }
+
+}
---
+++ tediouscatblog-web/src/test/java/com/tediouscat/tediouscatblog/web/TediouscatblogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tediouscat.tediouscatblog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TediouscatblogWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
