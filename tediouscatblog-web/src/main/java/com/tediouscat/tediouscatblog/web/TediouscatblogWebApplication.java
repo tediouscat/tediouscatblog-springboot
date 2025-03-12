acf86866289629cf8e28b3864210249e46c931b1
tediouscat <515889839@qq.com>
2025-03-13 00:21:00 +0800

    自定义注解日志切面

--- tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/TediouscatblogWebApplication.java
+++ tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/TediouscatblogWebApplication.java
@@ -2,8 +2,10 @@ package com.tediouscat.tediouscatblog.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.tediouscat.tediouscatblog.*"}) // 多模块项目中，必需手动指定扫描 com.tediouscat.tediouscatblog 包下面的所有类
 public class TediouscatblogWebApplication {
 
     public static void main(String[] args) {
---
+++ tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/model/User.java
@@ -0,0 +1,12 @@
+package com.tediouscat.tediouscatblog.web.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	// 用户名
+    private String username;
+    // 性别
+    private Integer sex;
+}
+
