a4ac6317744f5e0f9d5ca433998f4a9e6b6cc86f
tediouscat <tediouscat@126.com>
2025-03-13 14:19:34 +0800

    整合Knife4j

--- tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/controller/TestController.java
+++ tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/controller/TestController.java
@@ -5,6 +5,7 @@ import com.tediouscat.tediouscatblog.common.enums.ResponseCodeEnum;
 import com.tediouscat.tediouscatblog.common.exception.BizException;
 import com.tediouscat.tediouscatblog.common.utils.Response;
 import com.tediouscat.tediouscatblog.web.model.User;
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
 
     @PostMapping("/test")
