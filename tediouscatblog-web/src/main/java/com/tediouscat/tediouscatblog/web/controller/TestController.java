803512256721a3db616dabcdc40ddcf90a24c0ee
tediouscat <tediouscat@126.com>
2025-03-13 12:08:39 +0800

    参数校验v1

--- tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/controller/TestController.java
+++ tediouscatblog-web/src/main/java/com/tediouscat/tediouscatblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.tediouscat.tediouscatblog.web.controller;
 import com.tediouscat.tediouscatblog.common.aspect.ApiOperationLog;
 import com.tediouscat.tediouscatblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -14,9 +20,20 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
+        public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
         // 返参
-        return user;
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
