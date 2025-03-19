59274a5c9f62f4388d21400c37f78db64a634cb9
tediouscat <515889839@qq.com>
2025-03-19 20:30:27 +0800

    jwt使用数据库

--- tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/domain/mapper/UserMapper.java
+++ tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/domain/mapper/UserMapper.java
@@ -1,7 +1,13 @@
 package com.tediouscat.tediouscatblog.common.domain.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tediouscat.tediouscatblog.common.domain.dos.UserDO;
 
 public interface UserMapper extends BaseMapper<UserDO> {
+    default UserDO findByUsername(String username) {
+        LambdaQueryWrapper<UserDO> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserDO::getUsername, username);
+        return selectOne(wrapper);
+}
 }
--- tediouscatblog-module-jwt/src/main/java/com/tediouscat/tediouscatblog/jwt/service/UserDetailServiceImpl.java
+++ tediouscatblog-module-jwt/src/main/java/com/tediouscat/tediouscatblog/jwt/service/UserDetailServiceImpl.java
@@ -1,24 +1,37 @@
 package com.tediouscat.tediouscatblog.jwt.service;
 
+import com.tediouscat.tediouscatblog.common.domain.dos.UserDO;
+import com.tediouscat.tediouscatblog.common.domain.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 从数据库中查询
-        // ...
+        UserDO userDO = userMapper.findByUsername(username);
 
-		// 暂时先写死，密码为 quanxiaoha, 这里填写的密文，数据库中也是存储此种格式
+        // 判断用户是否存在
+        if (Objects.isNull(userDO)) {
+            throw new UsernameNotFoundException("该用户不存在");
+        }
+
 		// authorities 用于指定角色，这里写死为 ADMIN 管理员
-        return User.withUsername("tediouscat")
-                .password("$2a$10$xsMw.kvxQeHKU0ih/KtiMeDkfzSLJ6WtX5a22YLsClEZlL0NrgeKq")
+        return User.withUsername(userDO.getUsername())
+                .password(userDO.getPassword())
                 .authorities("ADMIN")
                 .build();
     }
