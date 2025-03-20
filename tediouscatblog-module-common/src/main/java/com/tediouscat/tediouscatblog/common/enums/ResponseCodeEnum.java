2b9ed055dc441987f436990b6abf4f71697413ea
tediouscat <tediouscat@126.com>
2025-03-20 15:42:10 +0800

    添加演示账号，仅支持查询操作

--- tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/enums/ResponseCodeEnum.java
+++ tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/enums/ResponseCodeEnum.java
@@ -17,6 +17,9 @@ public enum ResponseCodeEnum implements BaseExceptionInterface {
     PARAM_NOT_VALID("10001", "参数错误"),
     LOGIN_FAIL("20000", "登录失败"),
     USERNAME_OR_PWD_ERROR("20001", "用户名或密码错误"),
+    UNAUTHORIZED("20002", "无访问权限，请先登录！"),
+    FORBIDDEN("20004", "演示账号仅支持查询操作！"),
+
     ;
 
     // 异常码
--- tediouscatblog-module-jwt/src/main/java/com/tediouscat/tediouscatblog/jwt/service/UserDetailServiceImpl.java
+++ tediouscatblog-module-jwt/src/main/java/com/tediouscat/tediouscatblog/jwt/service/UserDetailServiceImpl.java
@@ -1,25 +1,56 @@
 package com.tediouscat.tediouscatblog.jwt.service;
 
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.tediouscat.tediouscatblog.common.domain.dos.UserDO;
+import com.tediouscat.tediouscatblog.common.domain.dos.UserRoleDO;
+import com.tediouscat.tediouscatblog.common.domain.mapper.UserMapper;
+import com.tediouscat.tediouscatblog.common.domain.mapper.UserRoleMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 从数据库中查询
-        // ...
+        UserDO userDO = userMapper.findByUsername(username);
 
-		// 暂时先写死，密码为 quanxiaoha, 这里填写的密文，数据库中也是存储此种格式
-		// authorities 用于指定角色，这里写死为 ADMIN 管理员
-        return User.withUsername("tediouscat")
-                .password("$2a$10$xsMw.kvxQeHKU0ih/KtiMeDkfzSLJ6WtX5a22YLsClEZlL0NrgeKq")
-                .authorities("ADMIN")
+        // 判断用户是否存在
+        if (Objects.isNull(userDO)) {
+            throw new UsernameNotFoundException("该用户不存在");
+        }
+
+        // 用户角色
+        List<UserRoleDO> roleDOS = userRoleMapper.selectByUsername(username);
+
+        String[] roleArr = null;
+
+        // 转数组
+        if (!CollectionUtils.isEmpty(roleDOS)) {
+            List<String> roles = roleDOS.stream().map(p -> p.getRole()).collect(Collectors.toList());
+            roleArr = roles.toArray(new String[roles.size()]);
+        }
+
+        // authorities 用于指定角色
+        return User.withUsername(userDO.getUsername())
+                .password(userDO.getPassword())
+                .authorities(roleArr)
                 .build();
     }
 }
