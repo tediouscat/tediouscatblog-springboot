4f51e80f898ee5df3816f4efe3893603cb7a0c5b
tediouscat <515889839@qq.com>
2025-03-29 20:00:01 +0800

    文章管理：文章发布接口开发（2）—— SQL 注入器实现批量插入

--- tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/domain/mapper/ArticleTagRelMapper.java
+++ tediouscatblog-module-common/src/main/java/com/tediouscat/tediouscatblog/common/domain/mapper/ArticleTagRelMapper.java
@@ -1,8 +1,9 @@
 package com.tediouscat.tediouscatblog.common.domain.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tediouscat.tediouscatblog.common.config.InsertBatchMapper;
 import com.tediouscat.tediouscatblog.common.domain.dos.ArticleTagRelDO;
 
-public interface ArticleTagRelMapper extends BaseMapper<ArticleTagRelDO> {
+public interface ArticleTagRelMapper extends InsertBatchMapper<ArticleTagRelDO> {
 
 }
