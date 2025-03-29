34413dd8707cd52af1dc46a42a9ded2958fec3df
tediouscat <515889839@qq.com>
2025-03-29 17:54:32 +0800

    整合 Mapstruct : 简化属性映射

---
+++ tediouscatblog-module-admin/src/main/java/com/tediouscat/tediouscatblog/admin/convert/BlogSettingsConvert.java
@@ -0,0 +1,22 @@
+package com.tediouscat.tediouscatblog.admin.convert;
+
+import com.tediouscat.tediouscatblog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
+import com.tediouscat.tediouscatblog.common.domain.dos.BlogSettingsDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BlogSettingsConvert {
+    /**
+     * 初始化 convert 实例
+     */
+    BlogSettingsConvert INSTANCE = Mappers.getMapper(BlogSettingsConvert.class);
+
+    /**
+     * 将 VO 转化为 DO
+     * @param bean
+     * @return
+     */
+    BlogSettingsDO convertVO2DO(UpdateBlogSettingsReqVO bean);
+
+}
